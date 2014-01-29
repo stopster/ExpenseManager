55d3f82163034cb439b67e2e1b4cc3c89656ffbc
Yuriy Husnay <husnaj23@gmail.com>
2014-01-29 22:57:52 +0200

    Snap.svg added

--- js/views/statistics/statistics.js
+++ js/views/statistics/statistics.js
@@ -1,11 +1,36 @@
 define([
     'underscore',
     'backbone.marionette',
+    'collections/expenses',
+    'collections/categories',
+
+    'views/statistics/statictics.categories.donut',
     'text!templates/statistics/statistics.tpl'
-], function(_, Marionette, Template) {
+], function(_, Marionette, ExpensesCollection, CategoriesCollection, CategoriesDonut, Template) {
     'use strict';
 
     return Marionette.ItemView.extend({
-        template : _.template(Template)
+        template : _.template(Template),
+
+        initialize : function() {
+            this.subViews = [
+                CategoriesDonut
+            ];
+
+            this.subViews = this.subViews.map(function(subView) {
+                console.log(subView, CategoriesDonut);
+                return new subView({
+                    categories : CategoriesCollection,
+                    expenses   : ExpensesCollection,
+                });
+            });
+        },
+
+        onShow : function() {
+            this.subViews.forEach(function(subView) {
+                console.log(subView.render());
+                this.$el.append(subView.render().$el);
+            }, this);
+        }
     });
 });
